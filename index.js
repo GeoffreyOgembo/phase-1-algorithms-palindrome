736538c97c95198c354b6717c4915870a062fcbb
GeoffreyOgembo <geoffereyogembo06@gmail.com>
2022-06-16 13:47:13 -0400

    pali

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word == word.split("").reverse().join("") ? true : false;
 }
+console.log(isPalindrome(mom))
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+console.log(isPalindrome("mom"))
+  
 if (require.main === module) {
   // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
